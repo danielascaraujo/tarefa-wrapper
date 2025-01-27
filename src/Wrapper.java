925c36018df68ec4a170312e854c9d4994e5fd00
Daniela Araujo <daniela.scaraujo@outlook.com>
2025-01-27 14:10:09 -0300

    First commit

---
+++ src/Wrapper.java
@@ -0,0 +1,11 @@
+public class Wrapper {
+
+    public static void main (String[] args) {
+
+        int idade = 33;
+        System.out.println(idade);
+
+       int idade2 = Integer.valueOf(34);
+        System.out.println(idade2);
+    }
+}
